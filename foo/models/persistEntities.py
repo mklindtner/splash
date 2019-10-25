0874b5813b2a072b42967385f561c693fe5f6984
mklindtner <mkl@mikiaholding.dk>
2019-10-25 13:36:06 +0200

    changed default values in html, changed some comments in db

--- foo/models/persistEntities.py
+++ foo/models/persistEntities.py
@@ -4,11 +4,12 @@ from foo import db
 def create_participant(username, email, comment, eventId):
     cur = e.User(username=username, email=email, comment=comment, eventId=eventId)
     #Yes this is trash, Yes this is a hackathon, Yes you need to be smart about the evaluation if the db grows
+    #also use try/except 
     allUsers = e.User.query.all()
 
     for user in allUsers:        
         if user.email == cur.email or user.username == cur.username:
-            return "email or username already exists"
+            return "email or username already exists" 
         
     db.session.add(cur)    
     db.session.commit()
@@ -16,10 +17,7 @@ def create_participant(username, email, comment, eventId):
 
 
 def get_participants(eventId):
-    # participants = db.session.execute(e.User.select()).where( e.User.columns.id == eventId)
-    # participants = db.session.query(e.User.eventId).get(eventId)
     participants = e.User.query.filter(e.User.eventId == eventId).all()
-    # db.session.
     return participants
 
 
